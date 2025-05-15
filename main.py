feb7d49d2a58edf5b0395d2b6b4bda6ba83c5239
APOORVKPATIDAR <130593347+APOORVKPATIDAR@users.noreply.github.com>
2025-05-15 14:08:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return"hello This is your buddy apoorv friend "
+    return"hello This is your buddy apoorv  "
 
 if __name__ =="__main__":
   app.run(host="0.0.0.0" ,port=5002)
